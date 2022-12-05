52e20dc896a97eb34629924a82b77d98dfe6c9e1
Mukhavertolet <Mukhavertolet@gmail.com>
2022-12-06 00:20:01 +0300

    it kinda works

--- game/GDN_GameJam_2022/Assets/Scripts/Chel.cs
+++ game/GDN_GameJam_2022/Assets/Scripts/Chel.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Chel : MonoBehaviour
 {
+    //greater powers
+    public GameManager gameManager;
+
+
+    //stats
+    public int id;
+
+
+
     //temporary world point, need to get this from game manager later
     public Vector3 movementCenter = Vector3.zero;
 
@@ -15,9 +24,11 @@ public class Chel : MonoBehaviour
     public float hungerDecreaseSpeed = 1;
 
     //state
-    private bool isHomeless = true;
-    private bool isBusy = false;
-    private bool isReady = false;
+    [SerializeField] private bool isHomeless = true;
+    [SerializeField] private bool isBusy = false;
+    [SerializeField] private bool isReady = false;
+
+    private bool isAllowedToWalk = true;
 
 
     //housing
@@ -28,6 +39,11 @@ public class Chel : MonoBehaviour
     //movement
     public float movementSpeed = 7.5f;
     public bool isWalking = false;
+    public float movementTimerMultiplier = 1;
+
+    //reproducing
+    public float excitementMultiplier = 1;
+
 
 
 
@@ -35,22 +51,69 @@ public class Chel : MonoBehaviour
     void Start()
     {
         hunger = maxHunger;
-        
+
+        StartCoroutine(RandomMoveTimer(movementTimerMultiplier));
+
+        StartCoroutine(RestoreReadiness(excitementMultiplier));
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && !isWalking)
-        {
-            Debug.Log("Command");
-            StartCoroutine(MoveToPoint(RandomPoint(movementCenter)));
-        }
-
 
         if (hunger <= 0)
             Death();
     }
+
+    private IEnumerator RestoreReadiness(float excitementMultiplier)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(7.5f, 12.5f) / excitementMultiplier);
+
+            isReady = true;
+            if (!isBusy && isReady)
+            {
+                StartCoroutine(Reproduce(excitementMultiplier));
+            }
+        }
+
+
+
+        yield return null;
+    }
+
+    private IEnumerator Reproduce(float excitementMultiplier)
+    {
+
+        if(AskForPair(this) != null)
+        {
+            isBusy = true;
+            //fuck
+            Debug.Log("hehe heha");
+            isReady = false;
+        }
+        else
+        {
+            //point and hearts go out
+            Debug.Log("EHEHOHUI(((");
+            yield return new WaitForSeconds(Random.Range(3f, 5f) * excitementMultiplier);
+        }
+
+        isBusy = false;
+
+
+        yield return null;
+    }
+
+    private Chel AskForPair(Chel whoAsked)
+    {
+        return gameManager.LookForPair(whoAsked);
+    }
+
+
+
 
     public void Death()
     {
@@ -61,7 +124,6 @@ public class Chel : MonoBehaviour
 
     private IEnumerator MoveToPoint(Vector2 point)
     {
-        Debug.Log("Moving...");
         isWalking = true;
         while (Vector2.Distance(transform.position, point) != 0)
         {
@@ -70,24 +132,46 @@ public class Chel : MonoBehaviour
 
         }
         isWalking = false;
-        Debug.Log("Moving finished");
         yield return null;
 
     }
 
     private Vector2 RandomPoint(Vector2 movementCenter)
     {
-        Debug.Log("Generating point...");
         Vector2 newPoint = new Vector2(Random.Range(movementCenter.x - 10, movementCenter.x + 10),
                                        Random.Range(movementCenter.y - 5, movementCenter.y + 5));
-
-        Debug.Log("New point = " + newPoint);
         return newPoint;
 
+    }
+
+    private IEnumerator RandomMoveTimer(float timeMultiplier)
+    {
+        while (true)
+        {
+            if (isAllowedToWalk && !isWalking)
+            {
+                StartCoroutine(MoveToPoint(RandomPoint(movementCenter)));
+            }
+
+            yield return new WaitForSeconds(Random.Range(0.75f, 5f) * timeMultiplier);
+
+        }
+
+
+        yield return null;
     }
 
 
 
 
+
+    public bool IsReady()
+    {
+        return isReady;
+    }
+    public bool IsBusy()
+    {
+        return isBusy;
+    }
 
 }
--- game/GDN_GameJam_2022/Assets/Scripts/GameManager.cs
+++ game/GDN_GameJam_2022/Assets/Scripts/GameManager.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public List<Chel> allChels;
+    private static GameManager instance;
 
+
+    public List<Chel> allChels;
+    private int lastChelID = 0;
 
 
 
@@ -16,22 +19,66 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
+        if (instance == null)
+            instance = new GameManager();
+        DontDestroyOnLoad(gameObject);
+
+
+
+
         GameObject[] firstTwoChels = GameObject.FindGameObjectsWithTag("Chel");
 
+
+
+        //shove it up some ugly cycle
         allChels.Add(firstTwoChels[0].GetComponent<Chel>());
         allChels.Add(firstTwoChels[1].GetComponent<Chel>());
+
+        firstTwoChels[0].GetComponent<Chel>().gameManager = gameObject.GetComponent<GameManager>();
+        firstTwoChels[1].GetComponent<Chel>().gameManager = gameObject.GetComponent<GameManager>();
+
+        firstTwoChels[0].GetComponent<Chel>().id = lastChelID + 1;
+        lastChelID += 1;
+        firstTwoChels[1].GetComponent<Chel>().id = lastChelID + 1;
+        lastChelID += 1;
+
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //foreach(Chel chel in allChels)
+        //{
+        //    if()
+        //}
+
+
+
     }
+
+
+    public Chel LookForPair(Chel ChelLookingForPair)
+    {
+        foreach (Chel chel in allChels)
+        {
+            if (!chel.IsBusy() && chel.IsReady() && chel.id != ChelLookingForPair.id)
+            {
+                return chel;
+            }
+        }
+
+        return null;
+
+    }
+
 }
